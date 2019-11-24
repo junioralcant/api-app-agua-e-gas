4802656bbc4a68a886264f5a686a3d9785d42a36
Junior Marques <junioralcant111@gmail.com>
2019-11-24 11:02:28 -0300

    Validaçao de permissao de usuario em categoria e pedido

--- src/app/controllers/CategoriaController.js
+++ src/app/controllers/CategoriaController.js
@@ -1,19 +1,41 @@
 const Categoria = require("../models/Categoria");
+const User = require("../models/User");
 
 class CategoriaController {
   async index(req, res) {
+    const userLogado = await User.findById(req.userId);
+
+    if (userLogado.provedor != true) {
+      return res.json({
+        mensagem: "Você não tem permissão para ver as categorias"
+      });
+    }
     const categoria = await Categoria.paginate();
 
     return res.json(categoria);
   }
 
   async store(req, res) {
+    const userLogado = await User.findById(req.userId);
+
+    if (userLogado.provedor != true) {
+      return res.json({
+        mensagem: "Você não tem permissão para cria categorias"
+      });
+    }
     const categoria = await Categoria.create(req.body);
 
     return res.json(categoria);
   }
 
   async update(req, res) {
+    const userLogado = await User.findById(req.userId);
+
+    if (userLogado.provedor != true) {
+      return res.json({
+        mensagem: "Você não tem permissão para atualizar categorias"
+      });
+    }
     const categoria = await Categoria.findByIdAndUpdate(
       req.params.id,
       req.body,
@@ -24,12 +46,26 @@ class CategoriaController {
   }
 
   async show(req, res) {
+    const userLogado = await User.findById(req.userId);
+
+    if (userLogado.provedor != true) {
+      return res.json({
+        mensagem: "Você não tem permissão para ver as categorias"
+      });
+    }
     const categoria = await Categoria.findById(req.params.id);
 
     return res.json(categoria);
   }
 
   async destroy(req, resp) {
+    const userLogado = await User.findById(req.userId);
+
+    if (userLogado.provedor != true) {
+      return res.json({
+        mensagem: "Você não tem permissão para deletar categorias"
+      });
+    }
     await Categoria.findByIdAndDelete(req.params.id);
 
     return resp.send();
--- src/app/controllers/PedidoController.js
+++ src/app/controllers/PedidoController.js
@@ -5,15 +5,25 @@ const User = require("../models/User");
 
 class PedidoController {
   async index(req, res) {
-    const pedidos = await Pedido.find({ cliente: req.userId }).populate(
-      "cliente"
-    );
+    const userLogado = await User.findById(req.userId);
+
+    // se não for um provedor
+    if (userLogado.provedor !== true) {
+      const pedidos = await Pedido.find({
+        cliente: req.userId
+      }).populate("cliente");
+
+      return res.json(pedidos);
+    }
+
+    const pedidos = await Pedido.find().populate("cliente");
 
     return res.json(pedidos);
   }
 
   async store(req, res) {
     const produto = await Produto.findById(req.body.produto);
+
     const pedido = await Pedido.create({
       ...req.body,
       cliente: req.userId,
@@ -24,7 +34,20 @@ class PedidoController {
   }
 
   async update(req, res) {
+    const userLogado = await User.findById(req.userId);
     const produto = await Produto.findById(req.body.produto);
+    const pedidoExistente = await Pedido.findById(req.params.id);
+
+    // validação para ver se o usuário logado fez o pedido ou é um provedor
+    if (
+      String(userLogado._id) !== String(pedidoExistente.cliente) &&
+      userLogado.provedor !== true
+    ) {
+      return res.json({
+        messagem: "Você não tem permissão para alterar este pedido"
+      });
+    }
+
     const pedido = await Pedido.findByIdAndUpdate(req.params.id, req.body, {
       valorTotal: produto.preco * req.body.quantidade,
       new: true
@@ -37,6 +60,14 @@ class PedidoController {
   }
 
   async show(req, res) {
+    const userLogado = await User.findById(req.userId);
+
+    if (!userLogado) {
+      return res.json({
+        mensagem: "Você não tem permissão para alterar esse pedido"
+      });
+    }
+
     const pedido = await Pedido.findById(req.params.id)
       .populate("produto")
       .populate("cliente");
